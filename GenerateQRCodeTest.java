6fa6e8b803d45824ca185dec391f98c70804cf9c
Sai Priya Mattaparthi <113373787+Saipriyamattaparthi@users.noreply.github.com>
2024-06-27 16:20:31 +0530

    Add files via upload

---
+++ GenerateQRCodeTest.java
@@ -0,0 +1,20 @@
+package Project.QR;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GenerateQRCodeTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
